956669f4315cee1e9549a27afed515f4703f01d7
Marco Cordero <marcko_385@hotmail.com>
2023-06-05 18:00:00 -0600

    Actividad 8
    Se sube la actividad 8 con el código opcional

---
+++ Actividades/Actividad 8/schedulerRR_mod.c
@@ -0,0 +1,67 @@
+/*
+* Código de planificador FCFS modificado para RR normal
+*/
+#include <scheduler.h>
+
+extern THANDLER threads[MAXTHREAD];
+extern int currthread;
+extern int blockevent;
+extern int unblockevent;
+
+QUEUE ready;
+QUEUE waitinginevent[MAXTHREAD];
+
+void scheduler(int arguments)
+{
+	int old,next;
+	int changethread=0;
+	int waitingthread=0;
+	
+	int event=arguments & 0xFF00;
+	int callingthread=arguments & 0xFF;
+
+	if(event==NEWTHREAD)
+	{
+		// Un nuevo hilo va a la cola de listos
+		threads[callingthread].status=READY;
+		_enqueue(&ready,callingthread);
+	}
+	
+	if(event==BLOCKTHREAD)
+	{
+
+		threads[callingthread].status=BLOCKED;
+		_enqueue(&waitinginevent[blockevent],callingthread);
+
+		changethread=1;
+	}
+
+	if(event==ENDTHREAD)
+	{
+		threads[callingthread].status=END;
+		changethread=1;
+	}
+	
+	if(event==UNBLOCKTHREAD)
+	{
+			threads[callingthread].status=READY;
+			_enqueue(&ready,callingthread);
+	}
+
+	if (event == TIMER) {
+		threads[callingthread].status=READY;
+		_enqueue(&ready,callingthread);
+
+		changethread=1;
+	}
+
+	if(changethread)
+	{
+		old=currthread;
+		next=_dequeue(&ready);
+		
+		threads[next].status=RUNNING;
+		_swapthreads(old,next);
+	}
+
+}
